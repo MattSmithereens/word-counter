d7b23a7c47fdb8daff793c805542039048879f16
Matt Smith <mattsmithereens@gmail.com>
2018-06-29 11:31:04 -0700

    started over.  Prev .csproj files were throwing errors during test attempts

---
+++ WordCounter.Tests/ModelTests/WordCountTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WordCounter.Models;
+using WordCounter;
+
+namespace WordCounter.Tests.ModelTests
+{
+    [TestClass]
+    public class WordCountTests
+    {
+        [TestMethod]
+        public void IsLeapYear_NumberDivisibleByFour_True()
+        {
+            // eventually your testing code will go here
+        }
+    }
+}
---
+++ WordCounter/Models/WordCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCounter.Models
+{
+    public class WordCount
+    {
+        public bool IsLeapYear(int year)
+        {
+            // logic will go here
+            return false;
+        }
+    }
+}
