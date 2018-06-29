b2feaebd17a747c8d779cbb1da2313e60b8c55db
Matt Smith <mattsmithereens@gmail.com>
2018-06-29 09:29:39 -0700

    establish dependencies and project folder framework

---
+++ WordCounter.Tests/ModelsTest/WordCountTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WordCounter.Tests.ModelsTest
+{
+    [TestClass]
+    public class WordCountTests
+    {
+        [TestMethod]
+        public test_result_return()
+        {
+        }
+    }
+}
---
+++ WordCounter/Models/WordCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCounter.Models
+{
+    public class WordCount
+    {
+        public WordCount()
+        {
+        }
+    }
+}
