661ae29b1b75332dfc3a3ed4b5fa6977835d4850
Matt Smith <mattsmithereens@gmail.com>
2018-06-30 22:23:53 -0700

    hopefully fixed version conflicts

--- WordCounter.Tests/ModelsTest/WordCountTests.cs
+++ WordCounter.Tests/ModelsTest/WordCountTests.cs
@@ -1,34 +1,74 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using WordCounter.Models;
+using WordCounter;
 
-namespace WordCounter.Tests.ModelsTest
+namespace WordCounter.Tests.ModelTests
 {
     [TestClass]
     public class WordCountTests
     {
         [TestMethod]
-        public void IsLeapYear_NumberDivisibleByFour_True()
+        public void GetWordString_ReturnWordString_String()
         {
-            // eventually your testing code will go here
+            //Arrange
+            string testString = "bang";
+            WordCount inputWord = new WordCount(testString);
+
+            //Act
+            string result = inputWord.GetWordString();
+
+            //Assert
+            Assert.AreEqual(testString, result);
         }
 
+        [TestMethod]
+        public void StringToLower_ReturnStringLower_String()
+        {
+            //Arrange
+            string testString = "BANG";
+            WordCount inputWord = new WordCount(testString);
+
+            //Act
+            string lowerTest = "bang";
+            string result = inputWord.StringToLower(testString);
 
 
+            //Assert
+            Assert.AreEqual(lowerTest, result);
+        }
+
+        //############## presently failing
+        [TestMethod]
+        public void GetSentenceString_ReturnSentenceString_String()
+        {
+            //Arrange
+            string testString = "bang bang bang";
+            WordCount inputWord = new WordCount(testString);
+
+            //Act
+            string result = inputWord.GetSentenceString();
+
+            //Assert
+            Assert.AreEqual(testString, result);
+        }
+
+        ////############### presently failing
         //[TestMethod]
-        //public void GetWordString_ReturnWordString_String()
+        //public void SentenceToLower_ReturnSentenceLower_String()
         //{
         //    //Arrange
-        //    string testString = "cat";
-        //    WordCounter inputWord = new WordCounter(testString);
+        //    string testString = "BANG bang";
+        //    WordCount inputWord = new WordCount(testString);
 
         //    //Act
-        //    string result = inputWord.GetWordString();
+        //    string lowerTest = "bang bang";
+        //    string result = inputWord.StringSentenceToLower(testString);
 
         //    //Assert
-        //    Assert.AreEqual(testString, result);
+        //    Assert.AreEqual(lowerTest, result);
         //}
     }
 }
--- WordCounter/Models/WordCount.cs
+++ WordCounter/Models/WordCount.cs
@@ -1,45 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace WordCounter.Models
 {
-    public class WordCounter
+    public class WordCount
     {
         private string _keyWordString;
         private string _compareWordString;
-        private int _keyCount = 0;
+        //private string[] compareWordArray;
+        //private int _keyCount = 0;
 
-        public bool IsLeapYear(int year)
+        public WordCount(string wordString = "")
         {
-            // logic will go here
-            return false;
+            _keyWordString = wordString;
         }
 
-        //public WordCounter(string wordString = "")
-        //{
-        //    _keyWordString = wordString;
-        //}
-
-        //public void SetWordString(string newWordString)
-        //{
-        //    _keyWordString = newWordString;
-        //}
-
-        //public string GetWordString()
-        //{
-        //    return _keyWordString;
-        //}
-
-        //public string StringToLower(string wordString)
-        //{
-        //    string wordLower = _keyWordString.ToLower();
-        //    return wordLower;
-        //}
-
-
-
+        public void SetWordString(string newWordString)
         {
+            _keyWordString = newWordString;
         }
+
+        public string GetWordString()
+        {
+            return _keyWordString;
+        }
+
+        public string StringToLower(string wordString)
+        {
+            string wordLower = _keyWordString.ToLower();
+            return wordLower;
+        }
+
+
+        //############ set sentence.  don't worry about breaking sentences up, just test word for word
+
+
+        public void SetSentenceString(string newSentenceString)
+        {
+           _compareWordString = newSentenceString;
+        }
+
+        public string GetSentenceString()
+        {
+            return _compareWordString;
+        }
+
+        //############## add tolower functionality later
+
+        //public string StringSentenceToLower(string newSentenceString)
+        //{
+        //    string sentenceLower = _compareWordString.ToLower();
+        //    return sentenceLower;
+        //}
+
+
+        //string s = "there is a cat";
+        // Split string on spaces.
+        // ... This will separate all the words.
+        //string[] words = s.Split(' ');
+        //foreach (string word in words)
+        //{
+        //    Console.WriteLine(word);
+        //}
+
+
+
     }
 }
